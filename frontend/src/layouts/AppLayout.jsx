8af2007a09f7f16e37d0baba8288dbbac635a2a4
leealswl <alswl54321600@gmail.com>
2025-08-11 22:32:46 +0900

    footer added

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer.jsx';
+import '../layouts/Footer.jsx'
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className="app-layout">
       <header>네비 추가</header>
 
       {/* 라우팅된 페이지가 여기에 렌더링됨 */}
-      <main>
+      <main className="app-main">
         <Outlet />
       </main>
 
--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import XIcon from '@mui/icons-material/X';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import '../styles/footer.style.css'
 
 const Footer = () => {
-  return (
-    <div>footer</div>
-  )
-}
+  
 
-export default Footer
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+
+        {/* About */}
+        <div className="footer-section">
+          <h4>
+            도서관 프로젝트
+          </h4>
+          <p>
+            오프라인 서점·도서관의 감성을<br />
+            온라인에서 그대로 느껴보세요.
+          </p>
+        </div>
+
+        {/* Service*/}
+        <div className="footer-section">
+          <h4>고객 서비스</h4>
+          <ul>
+            <li><a href="/">자주 묻는 질문</a></li>
+            <li><a href="/">이용 가이드</a></li>
+            <li><a href="/">문의하기</a></li>
+          </ul>
+        </div>
+
+        {/* Follow Us */}
+        <div className="footer-section">
+          <h4>Follow Us</h4>
+          <ul className="social-media">
+            <li><InstagramIcon /></li>
+            <li><YouTubeIcon /></li>
+            <li><XIcon /></li>
+            <li><GitHubIcon /></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div className="footer-bottom">
+        <p>&copy; 2025 도서관 프로젝트</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
