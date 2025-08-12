44c28d3ecd796f9c436a3709902e8966f24fffb9
leealswl <alswl54321600@gmail.com>
2025-08-12 18:09:57 +0900

    login api add

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const api = () => {
   return (
-    <div>api -axious 사용 </div>
+    <div>api -axious 사용 백엔드 baseurl -8070 그다음에
+      
+       프론트- 알라딘데이터 사용할거면 훅만들어서 사용하기 훅에서 api import  </div>
   )
 }
 
