8df9568d963fb613512f33eeb9b360f62511fea1
leealswl <alswl54321600@gmail.com>
2025-08-14 10:03:27 +0900

    resolve: unify axios config

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
 import React from 'react'
-import axios from 'axios'
-
-
-const api = axios.create({
-  baseURL: 'http://localhost:8070',         
-  timeout: 10000,
-  withCredentials: false, 
-=======
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8070",
   withCredentials: true,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
->>>>>>> mj
 });
 
 export default api;
