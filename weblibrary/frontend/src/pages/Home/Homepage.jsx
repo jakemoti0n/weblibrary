e1c947d24aa790c97f135248c34e914c91b7cfc2
jakemoti0n <jakem0tion@naver.com>
2025-08-24 16:53:29 +0900

    new

---
+++ weblibrary/frontend/src/pages/Home/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComicTable from './ComicTable.jsx';
+
+const Homepage = () => {
+  return (
+    <div>
+      <ComicTable />
+    </div>
+  )
+}
+
+export default Homepage
